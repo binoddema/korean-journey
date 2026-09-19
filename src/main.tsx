d1dec0727767d068e176aca6c83b7747d42f7147
binoddema <tiagobinod2002@gmail.com>
2026-09-19 17:44:17 +0200

    Import nuovo.css for additional styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { StoreProvider } from "./store";
 import "./styles.css";
+import "./nuovo.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
